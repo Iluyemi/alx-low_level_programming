f3730de193b3ec22ef33b533affd9bd45a8e743f
John Iluyemi <johniluyemi@gmail.com>
2022-06-30 22:14:12 +0000

    comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-int main(void)/*main function*/
+/**
+ * main - printing a string
+ * Return: 0 if exited properly
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
