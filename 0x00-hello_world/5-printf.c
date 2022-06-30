d4b554083086b57db6c453cc3a82c2eb9e23dd66
John Iluyemi <johniluyemi@gmail.com>
2022-06-30 22:29:31 +0000

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void) /*main function*/
+/**
+ * main - main function
+ * Return: 0 if exicted properly
+ */
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
