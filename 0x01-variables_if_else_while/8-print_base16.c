5ac73e932a3a2195f6761e45f8cf615d792e37aa
John Iluyemi <johniluyemi@gmail.com>
2022-07-02 22:10:20 +0000

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - the main function
+ * Return: 0 return value if successful
+ */
+int main(void)
+{
+char CH;
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar((i%10) + "0");
+}
+for (CH = 'a'; CH <= 'f'; CH++)
+{
+putchar(CH);
+}
+putchar('\n');
+return (0);
+}
+~    
