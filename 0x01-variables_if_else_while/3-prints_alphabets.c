05c336c8d45df7841a1e4c4ee9503c254b27a1be
John Iluyemi <johniluyemi@gmail.com>
2022-07-02 14:47:39 +0000

    Task3

---
+++ 0x01-variables_if_else_while/3-prints_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - the main function
+ * Return: 0 return value if successful
+ */
+int main(void)
+{
+char ch;
+char CH;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (CH = 'A'; CH <= 'Z'; CH++)
+{
+putchar(CH);
+}
+return (0);
+}
