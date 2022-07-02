24ba6ceb18e5fcd2705c4f3144522ee7fe02e854
John Iluyemi <johniluyemi@gmail.com>
2022-07-02 15:12:31 +0000

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - the main function
+ * Return: 0 if successful
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if(ch = 'q' || ch = 'e')
+{
+printf("");
+}
+else
+{
+putchar(ch);
+}
+}
+return (0);
+}
