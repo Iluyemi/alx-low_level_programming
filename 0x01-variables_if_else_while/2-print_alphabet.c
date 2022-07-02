22dea5387e4a8ac52b4d4dccdf24d52bd69622a9
John Iluyemi <johniluyemi@gmail.com>
2022-07-02 13:52:03 +0000

    Task 0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - the main function
+ * Return: 0 if code if successful
+ */
+int main(void)
+{
+char ch;
+for (char ch = 'a'; ch <= 'z'; ch++)
+{
+printf("%c", ch);
+}
+return (0);
+}
