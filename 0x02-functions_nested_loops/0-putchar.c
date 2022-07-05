15d42bdfadc130d454bb96d665917f10f0394b71
John Iluyemi <johniluyemi@gmail.com>
2022-07-05 21:47:52 +0000

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include "main.h"
+#include <unistd.h>
 /**
  * main - the main function
  * Return: 0 return value if successful
  */
 int main(void)
 {
-_putchar("_putchar");
+write(2, "_putchar", 59);
 return (0);
 }
