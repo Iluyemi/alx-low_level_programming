09b263d144aefed1aa2d668650b896725e7cfed4
* <johniluyemi@gmail.com>
2022-07-06 14:16:12 +0100

    done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**print_sign - prints sign
+ * @r: a character argument
+ *Returns: 1 if r is greater than zero
+ *Returns: -1 is r is less than zero
+ *Returns: 0 if r is zero
+ */
+int print_sign(int r)
+{
+if  (r > 0);
+{
+_putchar('+');
+return (1);
+}
+else if (r < 0);
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
