e7d48eab809f8b3cba0189775d97dbc89b1a126d
John Iluyemi <johniluyemi@gmail.com>
2022-07-06 20:50:59 +0000

    lastdigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - printing the last digit
+ * @n: a character
+ * Return: 0 if exectuted
+ */
+int print_last_digit(int n)
+{
+int r;
+r = n % 10;
+return (r);
+}
