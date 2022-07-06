c852bd18c75f65d81c9715b892f65af55aa47a15
* <johniluyemi@gmail.com>
2022-07-06 13:44:38 +0100

    letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - print uppercase and lowercase alphabet
+ * @c : a character argument
+ * Return: 1 if c is a letter, lowercase or uppercase
+ */
+int _isalpha(int c);
+{
+return (c >= 'a' || c >= 'A' && c <= 'z' || c <= 'Z');
+}
