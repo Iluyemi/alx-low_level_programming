2aa775d24b74aecc76cb0e8570759dd1de763655
John Iluyemi <johniluyemi@gmail.com>
2022-07-08 09:58:29 -0700

    done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * _isupper - the protyped function
- *   * @c: input character
- *    * Return: 1 if uppercase
- *     */
+ * _isupper - the protyped function
+ * @c: input character
+ * Return: 1 if uppercase
+ */
 int _isupper(int c)
 {
 return(c >= 'A' || c <= 'Z');
