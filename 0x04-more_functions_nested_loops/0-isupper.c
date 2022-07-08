3007ba18b904d3ea10c479da7e3d0f30335ebf67
* <johniluyemi@gmail.com>
2022-07-08 16:18:02 +0100

    loop

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - the protyped function
+ * @c : input character
+ * Return: 0 if not uppercase
+ */
+int _isupper(int c)
+{
+int c;
+if (c >= 'A' || c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
