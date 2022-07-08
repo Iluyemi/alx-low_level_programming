a5c7a6752bc5af7f86422cf440fd82a14655579a
John Iluyemi <johniluyemi@gmail.com>
2022-07-08 17:34:40 +0000

    loop

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,12 +4,16 @@
  * Return: 1 if c is digit otherwise 0
  * @c: a character
  */
+i
 int _isdigit(int c)
 {
-if (c >= 0 && c <= 9)
+if (c >= 0  &&  c <= 9)
 {
 return (1);
 }
 else
+{
 return (0);
+
+}
 }
