5726bba07b340b312c480e4e65b62783fb761032
John Iluyemi <johniluyemi@gmail.com>
2022-07-12 12:12:48 +0000

    swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - Swaps the value of an int
+ * Return: if successful
+ * @a: a pointer
+ * @b: a pointer
+ */
+void swap_int(int *a, int *b);
+{
+int p = *a;
+*a = *b;
+*b = p;
+return;
+}
--- 0x05-pointers_arrays_strings/test.c
+++ 0x05-pointers_arrays_strings/test.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
-void reset_to_98(int *n)
+void swap_int(int *a, int *b)
 {
-*n = 98;
+int p = *a;
+*a = *b;
+*b = p;
 return;
 }
 int main(void)
 {
-  int n;
+  int a;
+    int b;
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
+    a = 98;
+    b = 42;
+    printf("a=%d, b=%d\n", a, b);
+    swap_int(&a, &b);
+    printf("a=%d, b=%d\n", a, b);
     return (0);
 }
