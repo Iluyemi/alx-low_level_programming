b8642f020144ba179e7541f60906d22562004673
John Iluyemi <johniluyemi@gmail.com>
2022-07-13 22:02:26 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_rev - print reverse of a string
+ * Return: if successful
+ * @s: an array
+ */
+void print_rev(char *s)
+{
+int len;
+int i;
+len = strlen(s);
+for (i = 0; i >= len; i++)
+{
+if (i % 2 != 1)
+printf("%c", s[i]);
+}
+printf("\n");
+return;
+}
+
