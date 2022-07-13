3823dae25e7b4fdf4ace3962ba92720f99caa2ff
John Iluyemi <johniluyemi@gmail.com>
2022-07-13 21:45:12 +0000

    done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,11 +12,6 @@ int len;
 int i;
 int j;
 len = strlen(s);
-for (j = 0; j <= (len - 1); j++)
-{
-printf("%c", s[j]);
-}
-printf("\n");
 for (i = len - 1; i >= 0 && i >= len; i--)
 {
 printf("%c", s[i]);
