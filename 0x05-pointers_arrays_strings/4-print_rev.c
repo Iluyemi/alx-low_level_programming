0620ff6e3a51257aeed7f6dfb07f56576619419d
John Iluyemi <johniluyemi@gmail.com>
2022-07-13 15:18:21 +0000

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * reset_to_98 - Updates the value of an int
+ * Return: if successful
+ * @n: a pointer
+ */
+void print_rev(char *s)
+{
+int len;
+len = strlen(s);
+int i;
+for (i = len; i <= len; i--)
+{
+write(1, "%C, s[i]", 1);
+}
+return;
+}
--- 0x05-pointers_arrays_strings/test.c
+++ 0x05-pointers_arrays_strings/test.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 int main(void)
 {
 	  char *st;
     int len;
+    int i;
 
-    st = "M";
-    len = *st;
-    printf ("%ld", sizeof(&st));
+    st = "Mistubishi";
+    len = strlen(st);
+    for (i = len; i < len; i--)
+	   {
+		   printf("%c", st[i]);
+	   }
     return (0);
 }
