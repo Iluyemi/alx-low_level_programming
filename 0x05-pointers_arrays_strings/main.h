a61da7509652e7deb0d0acb6a19a551d38ca711f
John Iluyemi <johniluyemi@gmail.com>
2022-07-13 22:49:55 +0000

    done

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
-void reset_to_98(int *n);
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -8,3 +9,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+#endif
