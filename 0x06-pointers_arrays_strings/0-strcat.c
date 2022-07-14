39ef08266f82e25d277e13be7cdcdb7b3842491b
John Iluyemi <johniluyemi@gmail.com>
2022-07-14 07:51:57 +0000

    0x06

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * @dest: character
+ * @src: a character
+ * Return: if executed properly
+ */
+char *_strcat(char *dest, char *src)
+{
+char *s;
+s = strcat(dest, src);
+return (s);
+}
