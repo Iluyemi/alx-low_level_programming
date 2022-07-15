db1f8e02134ad8e803f7af63f8e55833633f5221
John Iluyemi <johniluyemi@gmail.com>
2022-07-15 22:19:36 +0000

    push

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,16 +2,15 @@
 #include <string.h>
 #include <stdio.h>
 /**
- * _strncpy - print a copy of character
- * @dest: first param
- * @src: second param
- * @n: 3rd param
- * Return: string
+ * reverse_array - print a copy of character
+ * @a: first param
+ * @n: second param
+ * Return: void
  */
 void reverse_array(int *a, int n)
 {
 int f;
-for (f = n; f <= n; f--)
+for (f = n - 1; f <= n; f--)
 {
 printf("%d", a[f]);	
 }
