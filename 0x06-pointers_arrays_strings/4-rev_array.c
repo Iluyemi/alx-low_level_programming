20c0293fbf0ab65ad012a59a763343f0f0adcc27
John Iluyemi <johniluyemi@gmail.com>
2022-07-15 22:15:21 +0000

    done

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - print a copy of character
+ * @dest: first param
+ * @src: second param
+ * @n: 3rd param
+ * Return: string
+ */
+void reverse_array(int *a, int n)
+{
+int f;
+for (f = n; f <= n; f--)
+{
+printf("%d", a[f]);	
+}
+return;
+}
