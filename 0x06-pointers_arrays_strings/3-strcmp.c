7cc6294d847b07ca394f759e885a56a643387f4c
John Iluyemi <johniluyemi@gmail.com>
2022-07-15 12:54:23 +0000

    done

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * strcmp - a function
+ * @s1: first param
+ * @s2: second param
+ * Return: string
+ */
+int _strcmp(char *s1, char *s2);
+{
+return (strcmp(s1, s2));
+}
