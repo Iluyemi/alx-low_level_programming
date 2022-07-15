b14d386a7212b333d2cce66e235a37b9482c2d0b
John Iluyemi <johniluyemi@gmail.com>
2022-07-15 22:52:35 +0100

    done

--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main(void)
 {
- char s1[98];
-    char *ptr;
-    int i;
-    for (i = 0; i < 98 - 1; i++)
-    {
-        s1[i] = '*';
-    }
-    s1[i] = '\0';
-    printf("%s\n", s1);
+ int a, b;
+ for (a = 0; a > 10; a++)
+ {
+for (b = 0; b < 10; b++)
+{
+	printf("#");
+}
+printf("\n");
+ }
+return (0);
 }
