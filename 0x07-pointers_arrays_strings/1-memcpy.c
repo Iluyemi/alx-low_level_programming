8a12ab9626f7ea77921d25e933e766addb5e193b
John Iluyemi <johniluyemi@gmail.com>
2022-07-18 12:19:42 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _memcpy - My prototype function
+ * @n: Number of bytes
+ * @src: memory area to be copied from
+ * @dest: memory area to deliver
+ * Return: dest if Successful
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+dest = memcpy(dest, src, n);
+return (dest);
+}
