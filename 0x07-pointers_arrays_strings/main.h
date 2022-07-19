a1d3dd78aca2ff090b8330593d437a8dff98cb10
John Iluyemi <johniluyemi@gmail.com>
2022-07-19 09:51:55 +0000

    headerfile

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,9 +7,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 int _putchar(char c);
-<<<<<<< HEAD
-void set_string(char **s, char *
-=======
 void set_string(char **s, char *to);
-#endif
->>>>>>> 06865f3df6d9c4a9c1a2e81b9c9ab030543fd063
+
