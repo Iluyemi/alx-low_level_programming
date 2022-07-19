644829f99d05099830db6cecfd1335f5ca0e0961
John Iluyemi <johniluyemi@gmail.com>
2022-07-19 23:36:01 +0000

    task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - prototype function
+ * @s: first character array
+ * Return: if executed properly
+ */
+void _puts_recursion(char *s)
+{
+int i = 0;
+int n = 0;
+while (s[i])
+i++;
+while (n < i)
+{
+_putchar(s[n]);
+n++;
+}
+}
