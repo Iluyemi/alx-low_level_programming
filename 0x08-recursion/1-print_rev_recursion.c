bcca0fd877de99fbe756200607a408a6e385491c
John Iluyemi <johniluyemi@gmail.com>
2022-07-20 06:31:18 -0700

    betty style

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * main - check the code
- *_print_rev_recursion: printing reverse
+ *_print_rev_recursion - printing reverse
+ *@s: input array
  * Return: Always 0.
  */
 void _print_rev_recursion(char *s)
 {
 if (*s != '\0')
 {
-_print_rev_recursion(s+1);
+_print_rev_recursion(s + 1);
 _putchar(*s);
 return;
 }
