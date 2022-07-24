3e4e29e56739f763ebb8f55831ce9f54298e6159
John Iluyemi <johniluyemi@gmail.com>
2022-07-24 23:23:50 +0000

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,13 @@
 #include "main.h"
+/**
+ *  main - Write a program that prints its name, followed by a new line
+ *  @argc: Argument counter
+ *  @argv: Argument vector
+ *  Return: if successful 1 otherwise 0
+ */
 int main(int argc, char *argv[])
 {
-	_putchar(*argv);
+	_putchar(argv);
 	_putchar("\n");
-
 return (0);
 }
