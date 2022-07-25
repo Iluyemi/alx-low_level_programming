e9fa78898e4aab8e27b1fad17d939f7d130e5659
John Iluyemi <johniluyemi@gmail.com>
2022-07-25 00:11:42 +0000

    Task1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: argument vector
  * Return: if successful, and 1 if not
  */
-int main(int argc, char *argv[])
+int main(int argc, char _attribute_((_unused_)) *argv[])
 {
 int i = 1;
 argc = argc - i;
