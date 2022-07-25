831ba8309d5d83cdd19d542bf6c388d739154c05
John Iluyemi <johniluyemi@gmail.com>
2022-07-25 15:59:14 +0000

    Task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	char *ar = malloc(size * sizeof(char));
+	char *ar = (char *)malloc(size * sizeof(char));
 
 	for (i = 0; i < size; i++)
 	{
