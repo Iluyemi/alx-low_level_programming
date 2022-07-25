3e1fd0bbe37d09ca18b242cc722bbe4e2e94f135
John Iluyemi <johniluyemi@gmail.com>
2022-07-25 00:36:08 +0000

    Task2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,7 +6,7 @@
  * @argv: argument vector
  * Return: if successful, and 1 if not
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int i;
 	for (i = 0; i < argc; i++)
