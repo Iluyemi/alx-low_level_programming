5fb20614dbab1804f1a0a8d8b0ee7021526e6980
John Iluyemi <johniluyemi@gmail.com>
2022-07-25 21:56:42 +0000

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - My Prototype function
+ * @s1: prototype array
+ * @s2: prototype array
+ * Return: NULL if size = 0
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j;
+
+	while (s1[i] && s2[j])
+	{
+		i++;
+		j++;
+	}
+	ar = malloc((i + j) * sizeof(char));
+if (ar == NULL)
+{
+	return (NULL);
+}
+for (n = 0; n < (i + j); n++)
+{
+	while (n < i)
+	{
+		ar[n] = s1[n];
+	}
+	while (n > i)
+	{
+		ar[n] = s2[n];
+	}
+	return (ar);
+}
