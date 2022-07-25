39d865d918b6b92ea8dee73e51f61dccbf5a3075
John Iluyemi <johniluyemi@gmail.com>
2022-07-25 00:33:20 +0000

    Task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - my main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: if successful, and 1 if not
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+
+	
+	}
+}
