8aa127debc84e588a0b5770a6f3cccb5a0713f51
John Iluyemi <johniluyemi@gmail.com>
2022-07-27 05:50:02 +0000

    0x0B-malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,22 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
+
+
+
 /**
- * _strdup - My Prototype function
- * @str: prototype array
- * Return: NULL if size = 0
- */
+* alloc_grid - returns a pointer to a
+*2 dimensional array of integers.
+*@width: width oàf array
+*@height: height of array
+*
+*Return: pointer to the array
+*/
+
 int **alloc_grid(int width, int height)
 {
-	int *i [][];
-if (width == 0 || height == 0)
-	return (NULL);
-i = malloc(width * height * sizeof(int));
-for (a = 0; a < height; a++)
-{
-	for (b = 0; b < width; b++)
+	int **array;
+	int i = 0, j;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+	array = (int **) malloc(sizeof(int *) * height);
+	if (array != NULL)
 	{
-		i[a][b] = 0;
+		for (; i < height; i++)
+		{
+			array[i] = (int *) malloc(sizeof(int) * width);
+			if (array[i] != NULL)
+			{
+				for (j = 0; j < width; j++)
+					array[i][j] = 0;
+			}
+			else
+			{
+				while (i >= 0)
+				{
+					free(array[i]);
+					i--;
+				}
+				free(array);
+				return (NULL);
+			}
+		}
+		return (array);
 	}
-}
-return (0);
+	else
+	{
+		return (NULL);
+	}
 }
