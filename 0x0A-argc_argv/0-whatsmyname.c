40452941c5c04557dd1ecee6d794143f69fb59e5
John Iluyemi <johniluyemi@gmail.com>
2022-07-27 10:03:41 +0100

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints multiplication of two numbers
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: returns 0
+*/
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	_putchar(argv[0]);
+	_putchar("\n");
+}
