961493afcf1ceecb3a9eac95fef83cf66786f331
John Iluyemi <johniluyemi@gmail.com>
2022-07-28 20:13:37 +0100

    termux to pc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+void *malloc_checked(unsigned int b)
+{
+	return (malloc(b));
+}
+int main(void)
+{
+unsigned int *p;
+printf("%u\n",*p);
+}
+
+
