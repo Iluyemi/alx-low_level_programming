e17e594732c134656adecb727b800e8a9232bb0b
John Iluyemi <johniluyemi@gmail.com>
2022-07-29 14:18:24 +0000

    0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _calloc - prototype function
+ * @nmemb: number for elements for the array
+ * @size: byte size
+ * Return: s if successful
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *array;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	array = malloc(nmemb * size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+	for (i = 0; i < nmemb; i++)
+	{
+	array[i] = 0;
+	}
+	return (array);
+	}
+}
