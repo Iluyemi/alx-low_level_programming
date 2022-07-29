515813db391d57bc31e5eaa5568534275d77796d
John Iluyemi <johniluyemi@gmail.com>
2022-07-29 13:48:48 +0000

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,31 +10,27 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, b = 0, j, a;
+	unsigned int i, b, j, a;
 	char *s;
 
+	i = 0;
+	b = 0;
+	j = 0;
 	while (s1[i])
 	{
 		i++;
 	}
-	j = 0;
 	while (s2[j])
 	{
 		j++;
 	}
 	if (n > j)
-	{
 		n = j;
-	}
 	s = malloc(((i + n) * sizeof(char)) + 1);
 	if (s1 == NULL)
-	{
 		s1 = " ";
-	}
 	if (s2 == NULL)
-	{
 		s2 = " ";
-	}
 	for (a = 0; a < (i + n); a++)
 	{
 	s[a] = s1[a];
