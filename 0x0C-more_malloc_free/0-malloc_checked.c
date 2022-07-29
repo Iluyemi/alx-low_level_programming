ee7642c6966a3f32ac183c038fc2de1a4684afbc
John Iluyemi <johniluyemi@gmail.com>
2022-07-29 11:00:06 +0000

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+/**
+ * malloc_checked - My prototype function
+ * @b: input unsigned int
+ * Return: pointer if successful and gives out 98 if not
+ */
 void *malloc_checked(unsigned int b)
 {
 	return (malloc(b));
+	if ((malloc(b)) == NULL)
+	{
+		exit(98);
+	}
 }
-int main(void)
-{
-unsigned int *p;
-printf("%u\n",*p);
-}
+
 
 
