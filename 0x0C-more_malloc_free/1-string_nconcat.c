8760abdd7cdd0b8234878bbafb9258bbb9953a37
John Iluyemi <johniluyemi@gmail.com>
2022-07-29 13:20:59 +0000

    0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * string_nconcat - prototype function
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ * Return: s if successful
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, b = 0, j, a;
+	char *s;
+
+	while (s1[i])
+	{
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		j++;
+	}
+	if (n > j)
+	{
+		n = j;
+	}
+	s = malloc(((i + n) * sizeof(char)) + 1);
+	if (s1 == NULL)
+	{
+		s1 = " ";
+	}
+	if (s2 == NULL)
+	{
+		s2 = " ";
+	}
+	for (a = 0; a < (i + n); a++)
+	{
+	s[a] = s1[a];
+	if (a >= i)
+	{
+	s[a] = s2[b];
+	b++;
+	}
+	}
+	s[i + n] = '\0';
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	return (s);
+}
