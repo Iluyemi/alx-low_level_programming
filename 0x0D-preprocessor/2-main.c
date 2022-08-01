85bfffd21bb9eb0beceaee7e03a9c7f6a82fb9e2
John Iluyemi <johniluyemi@gmail.com>
2022-08-01 18:29:17 +0000

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - my main function
+ * void: no input
+ * int: output
+ * Return: 0if successful
+ */
+
+int main(void)
+{
+_putchar(__FILE__);
+_putchar("\n");
+return (0);
+}
