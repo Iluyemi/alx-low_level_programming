eec658f9815cb246c0c12b1e163591c07c0ef675
John Iluyemi <johniluyemi@gmail.com>
2022-08-01 23:28:35 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - my data type
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
