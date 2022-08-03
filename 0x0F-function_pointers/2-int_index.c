2746459acb912c2ffe7c93e490f9c8c1cbf1f3fb
John Iluyemi <johniluyemi@gmail.com>
2022-08-03 16:29:24 +0000

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,10 +9,15 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+	if (array != NULL && cmp != NULL)
+	{
 	if (size <= 0)
 	{
 		return (-1);
 	}
 	for (i = 0; i < size; i++)
 	{
-
+		cmp(array[i]);
+	}
+	}
+}
