f66c0836258de601296d3c8fda5e0e212f153609
John Iluyemi <johniluyemi@gmail.com>
2022-08-03 09:18:02 +0000

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - My prototype function
+ * @name: first input
+ * @f: input function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *));
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - my function prototype
+ * @array: name of arrays 
+ * size: size of array
+ * action: input function
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+	int i;
+	for (i = 0; i <= size;i++)
+	{
+		action(array);
+		array + 1;
+	}
+
+}
+	
+
