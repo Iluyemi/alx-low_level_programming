ae6dc5f41696fdd18525b0bb4d152f60b90c1ac9
John Iluyemi <johniluyemi@gmail.com>
2022-08-11 22:11:12 +0000

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "list.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_list - printing list
+ * @h: node
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+size_t i;
+const list_t *head = h;
+while (head != NULL)
+{
+	if ((*head).str != NULL)
+	{
+		printf("[%d] %s\n", (*head).len, (*head).str);
+	}
+	else
+	{
+		printf("[%d] (nil)\n", (*head).len);
+	}
+	head = (*head).next;
+	i++;
+}
+	return (i);
+}
