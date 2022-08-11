9f5f772418265eff89f3b45b6f4c0987437c9267
John Iluyemi <johniluyemi@gmail.com>
2022-08-11 22:52:30 +0000

    0-print_list.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+list_t *add_node(list_t **head, const char *str)
+{
+	strdup
+list_t head []= list_t *head;
+(*head).str = str;
+(*head).len = strlen(str);
+while (head != NULL)
+{
+	printf("[%d] %s", (*head).len (*head).str);
+	(*head).next;
+}
+
+
+
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -17,4 +17,5 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif /*#ifndef _MAIN_H_*/
