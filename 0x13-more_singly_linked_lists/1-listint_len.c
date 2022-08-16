f4d5972f1c36839a46af2a6a65c58deac8069830
John Iluyemi <johniluyemi@gmail.com>
2022-08-16 15:06:38 +0000

    task1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - function to count
+ * @h: input struct
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+	while (h)
+	{
+		i++;
+		h->next;
+	}
+	return (i);
+}
+
