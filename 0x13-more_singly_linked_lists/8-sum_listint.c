4d9536fcaa78329949305a6956b64c30396c2d6a
John Iluyemi <johniluyemi@gmail.com>
2022-08-16 20:38:55 +0000

    debugging

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,8 +1,13 @@
 #include "lists.h"
-
+/**
+ * sum_listint - function
+ * @head: input
+ * Return: int
+ */
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
+
 	while (head)
 	{
 		if (head == NULL)
