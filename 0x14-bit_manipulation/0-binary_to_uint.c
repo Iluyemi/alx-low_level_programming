bad15c271828a841f7a7f50a1127105d367f591d
John Iluyemi <johniluyemi@gmail.com>
2022-08-19 11:31:48 +0000

    submit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,12 +9,10 @@ unsigned int binary_to_uint(const char *b)
 	int i, j;
 	int sum = 0;
 	int decimal = 1;
-	
+
 	j = strlen(b);
 	if (b == NULL)
-	{
 		return (0);
-	}
 	for (i = (j - 1); i >= 0; i--)
 	{
 		if (b[i] != '1' && b[i] != '0')
