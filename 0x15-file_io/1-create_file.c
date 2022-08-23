16097048bfd8eedaa85a23242d9809fb29f69d7c
John Iluyemi <johniluyemi@gmail.com>
2022-08-23 11:46:52 +0000

    submit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,18 +13,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR | O_EXCL, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
-	{
-		write(STDOUT_FILENO, "fails", 5);
 		return (-1);
-	}
-	else
-	{
 	if (text_content != NULL)
 		i = strlen(text_content);
 	write(fd, text_content, i);
 	close(fd);
-	}
 	return (1);
 }
