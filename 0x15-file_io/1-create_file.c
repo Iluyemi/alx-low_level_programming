a62bdf763df43dc70033a15a32bcbbc3615ea696
John Iluyemi <johniluyemi@gmail.com>
2022-08-23 10:24:41 +0000

    submit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * create_file - file to be created
+ * @filename: as the name suggest
+ * @text_content: input
+ * Return: 1 if successful
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_EXCL, 0600);
+	if (fd == -1)
+	{
+		write(STDOUT_FILENO, "fails", 5);
+		return (-1);
+	}
+	else
+	{
+	if (text_content != NULL)
+		i = strlen(text_content);
+	write(fd, text_content, i);
+	close(fd);
+	}
+	return (1);
+}
