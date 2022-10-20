1d52faddf636ab619dd6e12177c7043fe9e54253
John Iluyemi <johniluyemi@gmail.com>
2022-10-20 21:05:09 +0000

    task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function 
+ * @key: input 1
+ * @size: input 2
+ * Return : index
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	hash_node_t *new;
+	new->key =
+	new->value = 
+	new ->next = NULL;
+
+	hash_table_t
