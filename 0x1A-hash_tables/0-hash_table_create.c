216bca4686615bd42dd10fcda4079a4747c17b90
John Iluyemi <johniluyemi@gmail.com>
2022-10-20 20:22:06 +0000

    000

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - My prototpe function
+ * @size: input size of hash to create
+ * Return: the hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_node_t **link;
+	hash_table_t *new;
+	link = malloc(size * sizeof(hash_node_t *));
+	new = malloc(sizeof(hash_table_t));
+	new->size = size;
+	new->array = link;
+
+	return(new);
+}
+
