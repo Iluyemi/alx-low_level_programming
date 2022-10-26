11c2ee0cf26d7c293ea96980d0a92377ec9646fe
John Iluyemi <johniluyemi@gmail.com>
2022-10-26 14:01:49 +0100

    smiles

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,13 +11,17 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	char *hi;
 	hash_node_t *new;
 
+
+	if (key == NULL)
+		return NULL;
 	index = key_index((unsigned char *)key, ht->size);
 	hi = NULL;
 	for (new = ht->array[index]; new != NULL; new = new->next)
 	{
 		if (strcmp(new->key, key) == 0)
 		{
-			return (new->value);
+			hi = new->value;
+			return (hi);
 		}
 	}
 	return (hi);
